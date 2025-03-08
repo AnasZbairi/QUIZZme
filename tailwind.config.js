bdfcae33814f525d34282f7b70d5e721b9563a3c
AnasZbairi <ana96box2@gmail.com>
2025-03-08 18:07:41 +0100

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Include all JS/JSX/TS/TSX files in the src folder
+  ],
+  theme: {
+    extend: {}, // Add customizations here (e.g., colors, fonts, etc.)
+  },
+  plugins: [], // Add Tailwind plugins here (e.g., forms, typography, etc.)
+};
