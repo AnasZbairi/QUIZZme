84e9733c0f03a4cfaf2e3309dbbcc02a13a97e04
AnasZbairi <ana96box2@gmail.com>
2025-03-19 21:28:05 +0000

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import commonjs from '@rollup/plugin-commonjs';
 
 export default defineConfig({
-  plugins: [react(), commonjs()],
+  plugins: [react()],
   root: './',
   build: {
     outDir: 'dist',
     emptyOutDir: true,
+    rollupOptions: {
+      external: ['react-share'],
+    },
   },
 });
