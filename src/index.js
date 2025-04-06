af33d2d8396c8270631d98d2a2b876bb31635607
AnasZbairi <ana96box2@gmail.com>
2025-04-06 01:25:47 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './styles/index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css"; // Import the global styles
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// Create a root element and render the App component
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
