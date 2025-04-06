480e4f07aedc10083fc7d336606f3c241715ed60
AnasZbairi <ana96box2@gmail.com>
2025-04-06 01:29:58 +0000

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,9 @@
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-    "./public/index.html"
+    "./src/**/*.{js,jsx,ts,tsx}", // Include all JS/JSX/TS/TSX files in the src folder
   ],
   theme: {
-    extend: {},
+    extend: {}, // Add customizations here (e.g., colors, fonts, etc.)
   },
-  plugins: [],
-}
+  plugins: [], // Add Tailwind plugins here (e.g., forms, typography, etc.)
+};
